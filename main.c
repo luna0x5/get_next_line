acd0090b309f01faf5789230663a0e1c26b2923f
Moukit Hajar <hajarmoukit0x5@gmail.com>
2023-12-29 16:55:36 +0100

    with tests

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
-// #include "get_next_line.h"
-// #include <stdio.h>
-// #include <fcntl.h>
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <limits.h>
 // int main(void)
 // {
 // 	system("leaks a.out");
@@ -52,4 +53,25 @@
 // 	s = get_next_line(fd);
 // 	printf("%d:=> %s", i, s);
 // 	free(s);
+// }
+
+// int main(void)
+// {
+// 	system("leaks a.out");
+// 	char *s = get_next_line(42);
+// 	char *s1 = get_next_line(1337);
+// 	char *s2 = get_next_line(-99);
+// 	char *s3 = get_next_line(3);
+// 	char *s4 = get_next_line(OPEN_MAX);
+// 	char *s5 = get_next_line(OPEN_MAX + 1);
+// 	char *s6 = get_next_line(INT_MAX);
+// 	char *s7 = get_next_line(INT_MIN);
+// 	printf("fd = 42:=> %s\n", s);
+// 	printf("fd = 1337:=> %s\n", s1);
+// 	printf("fd = -99:=> %s\n", s2);
+// 	printf("fd = 3:=> %s\n", s3);
+// 	printf("fd = OPEM_MAX:=> %s\n", s4);
+// 	printf("fd = OPEN_MAX + 1:=> %s\n", s5);
+// 	printf("fd = INT_MAX:=> %s\n", s6);
+// 	printf("fd = INT_MIN:=> %s\n", s7);
 // }
--- main_bonus.c
+++ main_bonus.c
@@ -1,13 +1,13 @@
-// #include "get_next_line_bonus.h"
-// #include <stdio.h>
-// #include <fcntl.h>
+#include "get_next_line_bonus.h"
+#include <stdio.h>
+#include <fcntl.h>
 // int main(void)
 // {
 // 	system("leaks a.out");
 // 	int fd = open("test.txt", O_CREAT | O_RDWR, 0777);
 // 	int fd1 = open("test1.txt", O_CREAT | O_RDWR, 0777);
-// 	write(fd, "hello this is a test\nthe first writing was a line and this another one\nand this is the third\nand the fourth\n\n\n\n\n", 112);
-// 	write(fd1, "hello this is a test\nthe first writing was a line and this another one\nand this is the third\nand the fourth\n\n\n\n\n", 112);
+// 	// write(fd, "hello this is a test\nthe first writing was a line and this another one\nand this is the third\nand the fourth\n\n\n\n\n", 112);
+// 	// write(fd1, "hello this is a test\nthe first writing was a line and this another one\nand this is the third\nand the fourth\n\n\n\n\n", 112);
 // 	close(fd);
 // 	close(fd1);
 // 	fd = open("test.txt", O_RDWR, 0777);
@@ -44,8 +44,12 @@
 // 	char *s1 = get_next_line(1337);
 // 	char *s2 = get_next_line(-99);
 // 	char *s3 = get_next_line(3);
+// 	char *s4 = get_next_line(OPEN_MAX);
+// 	char *s5 = get_next_line(OPEN_MAX + 1);
 // 	printf("fd = 42:=> %s\n", s);
 // 	printf("fd = 1337:=> %s\n", s1);
 // 	printf("fd = -99:=> %s\n", s2);
-// 	printf("fd =: -99:=> %s\n", s3);
+// 	printf("fd = 3:=> %s\n", s3);
+// 	printf("fd = OPEN_MAX:=> %s\n", s4);
+// 	printf("fd = OPEN_MAX + 1:=> %s\n", s5);
 // }
